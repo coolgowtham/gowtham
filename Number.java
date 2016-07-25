76fa63a5b7f9fbb9af1e497a4b8a85d92669eed1
coolgowtham <gowthamscse12@gmail.com>
2016-07-25 09:45:23 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,17 @@
+public class Number {
+	public static void main(String[] args) {
+		int n=Integer.parseInt(args[0]);
+		if(n==0)
+		{
+			System.out.println("Zero");
+		}
+		else if(n>0)
+		{
+			System.out.println("positive");
+		}
+		else
+			System.out.println("negative");
+
+	}
+
+}
